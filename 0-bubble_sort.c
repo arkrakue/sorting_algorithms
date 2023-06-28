7f3045cc74dff7c898f554e774481b8125cdf27f
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-06-28 00:57:49 +0000

    Bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,26 +11,22 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp;
-	int swapped;
+	size_t i = 0, j = 0;
+	int aux = 0;
 
-	for (i = 0; i < size - 1; i++)
+	if (array == NULL || size == 0)
+		return;
+	for (; i < size - 1; i++)
 	{
-		swapped = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				swapped = 1;
+				aux = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = aux;
 				print_array(array, size);
 			}
 		}
-
-	if (swapped == 0)
-		break;
 	}
 }
