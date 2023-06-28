ce84fd3ecea1b96dccfa88962539b53cf7e858d9
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-06-28 01:37:43 +0000

    Header file Update

--- sort.h
+++ sort.h
@@ -28,4 +28,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+/************* QUICK SORT FUNCTIONS *******************/
+int partition(int *array, int low, int high, size_t size);
+void quicksort(int *array, int low, int high, size_t size);
+void quick_sort(int *array, size_t size);
 #endif
